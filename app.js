77c62801e79f9786e63869b6e0c2ab6b1c8d387b
Tanishka Jain <jain.tanishka0109@gmail.com>
2025-07-25 16:28:05 +0530

    index.js in app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const path = require("path");
 
+const indexRoutes = require("./routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+
+app.use("/", indexRoutes);
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
