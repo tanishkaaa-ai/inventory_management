1509c130c4baad6d69b5c7fa93e941984cd75454
Shlesha5847 <147795373+Shlesha5847@users.noreply.github.com>
2025-07-25 17:23:58 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const path = require("path");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
 
 const indexRoutes = require("./routes/index");
 
@@ -10,6 +12,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+require("dotenv").config();
+app.use(
+    expressSession({
+        resave:false, // 
+        saveUninitialized : false, 
+        secret : process.env.EXPRESS_SESSION_SECRET,
+    })
+);
+app.use(flash());
 
 app.use("/", indexRoutes);
 
