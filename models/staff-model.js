973cba98d08ba2df1149766474c3c21fcb059a1a
Tanishka Jain <jain.tanishka0109@gmail.com>
2025-07-25 16:39:18 +0530

    changes in staff

--- models/staff-model.js
+++ models/staff-model.js
@@ -4,7 +4,6 @@ const StaffSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Hashed
-    assignedCategory: { type: String }, // Optional: used to limit their scope
     createdAt: { type: Date, default: Date.now },
     lastLogin: { type: Date },
     isActive: { type: Boolean, default: true }
