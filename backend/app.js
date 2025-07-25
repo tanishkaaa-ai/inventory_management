a5191780225d0e27f6186e169e3b9cbbc17e8e67
Tanishka Jain <jain.tanishka0109@gmail.com>
2025-07-26 00:35:57 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -7,17 +7,18 @@ const flash = require("connect-flash");
 
 const indexRoutes = require("./routes/index");
 const staffrouter = require("./routes/staffRouter");
+const adminrouter = require("./routes/adminRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 require("dotenv").config();
 app.use(
     expressSession({
         resave:false, // 
-        saveUninitialized : false, 
+        saveUninitialized : true, 
         secret : process.env.EXPRESS_SESSION_SECRET,
     })
 );
@@ -25,6 +26,7 @@ app.use(flash());
 
 app.use("/", indexRoutes);
 app.use("/staff",staffrouter);
+app.use("/admin",adminrouter);
 
 app.listen(5000, () => {
     console.log("server running on port 5000");
