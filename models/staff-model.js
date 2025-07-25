33de5b0753219078ebcf98cd4a89447f26edd2ec
Tanishka Jain <jain.tanishka0109@gmail.com>
2025-07-25 18:11:41 +0530

    minute change

--- models/staff-model.js
+++ models/staff-model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const StaffSchema = new mongoose.Schema({
+const staffSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Hashed
