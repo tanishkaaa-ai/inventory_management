cd00a004a0d5316a58cd52f93bdc7a8a2db693fd
Shlesha5847 <147795373+Shlesha5847@users.noreply.github.com>
2025-07-26 23:48:13 +0530

    Create StaffDashboard.jsx

---
+++ frontend/src/pages/StaffDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StaffDashboard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Staff Dashboard</h1>
+    </div>
+  );
+};
+
+export default StaffDashboard;
