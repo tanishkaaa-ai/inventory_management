79304ba25c39cbca366f5a267cf1026a01a9618d
Tanishka Jain <jain.tanishka0109@gmail.com>
2025-07-26 23:46:05 +0530

    log works

--- backend/models/inventoryLog-model.js
+++ backend/models/inventoryLog-model.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+require("../models/admin-model"); // or wherever your model is
+
 
 const InventoryLogSchema = new mongoose.Schema({
     product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
     action: { type: String, enum: ["add", "remove", "update"], required: true },
     quantity: { type: Number, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "admin", required: true },
     timestamp: { type: Date, default: Date.now },
     remarks: { type: String }
 });
