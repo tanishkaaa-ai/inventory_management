d0eb5caee9b368fc31e633adc0f3fb9051162075
Shlesha5847 <147795373+Shlesha5847@users.noreply.github.com>
2025-09-16 21:51:02 +0530

    Update product-model.js

--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -12,4 +12,4 @@ const ProductSchema = new mongoose.Schema({
     timestamps:true
 });
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('product', ProductSchema);
